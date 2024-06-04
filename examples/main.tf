61b9212b3db06500bbae201e20aa8e9d7dd3458a
I584814 <diya.dhandapani.shanmugam@sap.com>
2024-06-04 14:17:13 +0530

    removed tf config files

--- examples/main.tf
+++
@@ -1,8 +0,0 @@
-# data "ias_application" "name" {
-#   id = "c5c483bb-62de-444e-b784-ded7d369eabd"
-# }
-
-resource "ias_application" "terraform_test" {
-  name = "terraform_test"
-  description = "sample application"
-}
