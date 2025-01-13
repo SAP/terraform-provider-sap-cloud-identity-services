e7ff0bb58a0bdb1fce921d3b56216047fc5472d3
I584814 <diya.dhandapani.shanmugam@sap.com>
2025-01-13 18:10:31 +0000

    provider test fixes

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -128,6 +128,9 @@ func TestIasProvider_AllResources (t *testing.T){
 
 	expectedResources := []string{
 		"ias_application",
+		"ias_user",
+		"ias_group",
+		"ias_schema",
 	}
 
 	ctx := context.Background()
@@ -149,6 +152,12 @@ func TestIasProvider_AllDataSources (t *testing.T){
 	expectedDataSources := []string{
 		"ias_application",
 		"ias_applications",
+		"ias_user",
+		"ias_users",
+		"ias_group",
+		"ias_groups",
+		"ias_schema",
+		"ias_schemas",
 	}
 
 	ctx := context.Background()
