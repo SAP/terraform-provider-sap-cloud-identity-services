5ff3fdf4f511ffefe680028c2838fd0ff94e162c
prajin-op <prajin.op@gmail.com>
2025-06-11 17:14:30 +0530

    test: enhance provider tests with OAuth2 and P12 certificate support

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -2,19 +2,21 @@ package provider
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
+	"testing"
 
 	"strconv"
-	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
+	tfresource "github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/dnaeon/go-vcr.v3/cassette"
 	"gopkg.in/dnaeon/go-vcr.v3/recorder"
@@ -25,20 +27,8 @@ type User struct {
 	Password string
 }
 
-func providerConfig(_ string, testUser User) string {
-	tenantUrl := "https://iasprovidertestblr.accounts400.ondemand.com/"
-	return fmt.Sprintf(`
-	provider "sci" {
-		tenant_url = "%s"
-		username = "%s"
-		password = "%s"
-	}
-	`, tenantUrl, testUser.Username, testUser.Password)
-}
-
 func getTestProviders(httpClient *http.Client) map[string]func() (tfprotov6.ProviderServer, error) {
 	sciProvider := NewWithClient(httpClient)
-
 	return map[string]func() (tfprotov6.ProviderServer, error){
 		"sci": providerserver.NewProtocol6WithError(sciProvider),
 	}
@@ -46,33 +36,30 @@ func getTestProviders(httpClient *http.Client) map[string]func() (tfprotov6.Prov
 
 func setupVCR(t *testing.T, cassetteName string) (*recorder.Recorder, User) {
 	t.Helper()
-
 	mode := recorder.ModeRecordOnce
 	if testRecord, _ := strconv.ParseBool(os.Getenv("TEST_RECORD")); testRecord {
 		mode = recorder.ModeRecordOnly
 	}
-
 	rec, err := recorder.NewWithOptions(&recorder.Options{
 		CassetteName:       cassetteName,
 		Mode:               mode,
 		SkipRequestLatency: true,
 		RealTransport:      http.DefaultTransport,
 	})
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	var testUser User
 	if rec.IsRecording() {
-		t.Logf("ATTENTION: Recording '%s'", cassetteName)
+		t.Logf("Recording new interactions in '%s'", cassetteName)
 		testUser.Username = os.Getenv("SCI_USERNAME")
 		testUser.Password = os.Getenv("SCI_PASSWORD")
-		if len(testUser.Username) == 0 || len(testUser.Password) == 0 {
-			t.Fatal("Env vars SCI_USERNAME and SCI_PASSWORD are required when recording test fixtures")
+		if testUser.Username == "" || testUser.Password == "" {
+			t.Fatal("SCI_USERNAME and SCI_PASSWORD must be set for recording")
 		}
 	} else {
-		t.Logf("Replaying '%s'", cassetteName)
-	}
-
-	if err != nil {
-		t.Fatal()
+		t.Logf("Replaying cassette '%s'", cassetteName)
 	}
 
 	rec.SetMatcher(requestMatcher(t))
@@ -86,31 +73,25 @@ func requestMatcher(t *testing.T) cassette.MatcherFunc {
 		if r.Method != i.Method || r.URL.String() != i.URL {
 			return false
 		}
-
-		bytes, err := io.ReadAll(r.Body)
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			t.Fatal("Unable to read body from request")
+			t.Fatal("Unable to read request body")
 		}
-
-		requestBody := string(bytes)
-		return requestBody == i.Body
+		return string(body) == i.Body
 	}
 }
 
 func redactAuthorizationToken() recorder.HookFunc {
 	return func(i *cassette.Interaction) error {
-
 		redact := func(headers map[string][]string) {
 			for key := range headers {
 				if strings.Contains(strings.ToLower(key), "authorization") {
-					headers[key] = []string{"redacted"}
+					headers[key] = []string{"REDACTED"}
 				}
 			}
 		}
-
 		redact(i.Request.Headers)
 		redact(i.Response.Headers)
-
 		return nil
 	}
 }
@@ -122,30 +103,23 @@ func stopQuietly(rec *recorder.Recorder) {
 }
 
 func TestSciProvider_AllResources(t *testing.T) {
-
 	expectedResources := []string{
 		"sci_application",
 		"sci_user",
 		"sci_group",
 		"sci_schema",
 	}
-
 	ctx := context.Background()
-	registeredResources := []string{}
-
+	var registeredResources []string
 	for _, resourceFunc := range New().Resources(ctx) {
-		var resp resource.MetadataResponse
-
-		resourceFunc().Metadata(ctx, resource.MetadataRequest{ProviderTypeName: "sci"}, &resp)
-
+		var resp tfresource.MetadataResponse
+		resourceFunc().Metadata(ctx, tfresource.MetadataRequest{ProviderTypeName: "sci"}, &resp)
 		registeredResources = append(registeredResources, resp.TypeName)
 	}
-
 	assert.ElementsMatch(t, expectedResources, registeredResources)
 }
 
 func TestSciProvider_AllDataSources(t *testing.T) {
-
 	expectedDataSources := []string{
 		"sci_application",
 		"sci_applications",
@@ -156,17 +130,62 @@ func TestSciProvider_AllDataSources(t *testing.T) {
 		"sci_schema",
 		"sci_schemas",
 	}
-
 	ctx := context.Background()
-	registeredDataSources := []string{}
-
-	for _, datasourceFunc := range New().DataSources(ctx) {
+	var registeredDataSources []string
+	for _, dsFunc := range New().DataSources(ctx) {
 		var resp datasource.MetadataResponse
-
-		datasourceFunc().Metadata(ctx, datasource.MetadataRequest{ProviderTypeName: "sci"}, &resp)
-
+		dsFunc().Metadata(ctx, datasource.MetadataRequest{ProviderTypeName: "sci"}, &resp)
 		registeredDataSources = append(registeredDataSources, resp.TypeName)
 	}
-
 	assert.ElementsMatch(t, expectedDataSources, registeredDataSources)
+}
+
+func TestAccSciProvider_withOAuth2(t *testing.T) {
+	rec, _ := setupVCR(t, "provider_oauth")
+	defer stopQuietly(rec)
+
+	clientID := os.Getenv("SCI_CLIENT_ID")
+	clientSecret := os.Getenv("SCI_CLIENT_SECRET")
+	if clientID == "" || clientSecret == "" {
+		t.Skip("SCI_CLIENT_ID and SCI_CLIENT_SECRET must be set")
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: getTestProviders(rec.GetDefaultClient()),
+		Steps: []resource.TestStep{{
+			Config: fmt.Sprintf(`
+			provider "sci" {
+			  tenant_url    = "https://iasprovidertestblr.accounts400.ondemand.com/"
+			  client_id     = "%s"
+			  client_secret = "%s"
+			}`, clientID, clientSecret),
+		}},
+	})
+}
+
+func TestAccSciProvider_withP12(t *testing.T) {
+	rec, _ := setupVCR(t, "provider_p12")
+	defer stopQuietly(rec)
+
+	content, err := os.ReadFile("test-fixtures/cert.p12")
+	if err != nil {
+		t.Fatalf("failed to read cert.p12: %v", err)
+	}
+	base64Content := base64.StdEncoding.EncodeToString(content)
+	password := os.Getenv("SCI_P12_PASSWORD")
+	if password == "" {
+		t.Skip("SCI_P12_PASSWORD must be set")
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: getTestProviders(rec.GetDefaultClient()),
+		Steps: []resource.TestStep{{
+			Config: fmt.Sprintf(`
+			provider "sci" {
+			  tenant_url               = "https://iasprovidertestblr.accounts400.ondemand.com/"
+			  p12_certificate_content  = "%s"
+			  p12_certificate_password = "%s"
+			}`, base64Content, password),
+		}},
+	})
 }
