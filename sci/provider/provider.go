75ae37349d9b2f1e167aa763e6c3c0fb3448901d
I584814 <diya.dhandapani.shanmugam@sap.com>
2025-09-04 18:58:10 +0000

    fix: throw an error for missing auth credentials

--- sci/provider/provider.go
+++ sci/provider/provider.go
@@ -202,6 +202,7 @@ func (p *SciProvider) Configure(ctx context.Context, req provider.ConfigureReque
 			},
 		}
 		cert = &tlsCert
+
 	} else {
 		httpClient = p.httpClient
 	}
@@ -233,6 +234,7 @@ func (p *SciProvider) Configure(ctx context.Context, req provider.ConfigureReque
 			return
 		}
 		client.AuthorizationToken = "Bearer " + token
+
 	} else if cert == nil {
 		// Fallback to Basic Auth (username + password)
 		var username, password string
@@ -248,7 +250,11 @@ func (p *SciProvider) Configure(ctx context.Context, req provider.ConfigureReque
 			password = config.Password.ValueString()
 		}
 
-		client.AuthorizationToken = "Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))
+		if username != "" && password != "" {
+			client.AuthorizationToken = "Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))
+		} else {
+			resp.Diagnostics.AddError("Authentication Details Missing", "Please provide either : \n- client_id and client_secret for OAuth2 Authentication \n- p12_certificate_content and p12_certificate_password for X.509 Authentication \n- username and password for Basic Authentication")
+		}
 	}
 
 	if resp.Diagnostics.HasError() {
