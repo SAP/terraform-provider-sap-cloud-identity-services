b9a04901c26fad3fa6ff5d88af6b7d2dc5f4b72e
Christian Lechner <22294087+lechnerc77@users.noreply.github.com.>
2025-12-08 11:59:58 +0100

    docs: reformat examples

--- docs/resources/group.md
+++ docs/resources/group.md
@@ -14,17 +14,17 @@ Creates a group in the SAP Cloud Identity Services.
 ```terraform
 # Create a basic group in SAP Cloud Identity Services
 resource "sci_group" "basic_group" {
-    display_name = "My Basic Group"
-    group_members = [
-      {
-        value = "valid-uuid",
-        type = "User"                       # Refer to the documentation for valid values
-      }
-    ]
-    group_extension = {
-      name = "Terraform"
-      description = "Group for terraform users"
+  display_name = "My Basic Group"
+  group_members = [
+    {
+      value = "valid-uuid",
+      type  = "User" # Refer to the documentation for valid values
     }
+  ]
+  group_extension = {
+    name        = "Terraform"
+    description = "Group for terraform users"
+  }
 }
 ```
 
--- examples/resources/sci_user/resource.tf
+++ examples/resources/sci_user/resource.tf
@@ -1,38 +1,38 @@
 # Create a user in SAP Cloud Identity Services
 resource "sci_user" "new_user" {
-  user_name   = "jdoe"
+  user_name = "jdoe"
   emails = [
     {
-      value = "john.doe@sap.com",
-      type  = "work"
+      value   = "john.doe@sap.com",
+      type    = "work"
       primary = true
     }
   ]
   name = {
-    family_name = "John"
-    given_name  = "Doe"
+    family_name      = "John"
+    given_name       = "Doe"
     honorific_prefix = "Mr."
   }
-  initial_password = "1234"       # Must be a valid password 
-  display_name = "John Doe"
-  user_type = "customer"          # Refer to the documentation for valid values
-  active = false
+  initial_password = "1234" # Must be a valid password 
+  display_name     = "John Doe"
+  user_type        = "customer" # Refer to the documentation for valid values
+  active           = false
   sap_extension_user = {
-    send_mail = false
+    send_mail     = false
     mail_verified = true
-    status = "active"             # Refer to the documentation for valid values
+    status        = "active" # Refer to the documentation for valid values
   }
 }
 
 
 # Create a user in SAP Cloud Identity Services with customSchemas
 resource "sci_user" "new_user" {
-  schemas = [                    # If custom schemas are to be used, ensure a valid schema ID is provided
+  schemas = [ # If custom schemas are to be used, ensure a valid schema ID is provided
     "urn:ietf:params:scim:schemas:core:2.0:User",
     "urn:ietf:params:scim:schemas:extension:sap:2.0:User",
     "urn:custom:SCI:1.0:User"
   ]
-  user_name   = "jdoe"
+  user_name = "jdoe"
   emails = [
     {
       value = "john.doe@sap.com",
