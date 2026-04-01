fd5a4b9d563e9425fce93d631b75f0b3fa0fe178
ravi-ydv <raviyadavrr37@gmail.com>
2026-04-01 06:25:06 +0000

    docs: add examples for sci_corporate_idp

---
+++ examples/data-sources/sci_corporate_idp/data-source.tf
@@ -0,0 +1,4 @@
+# Read a Corporate Identity Provider by ID
+data "sci_corporate_idp" "by_id" {
+  id = "dd005d8b-1fee-4e6b-b6ff-cb9a197b7fe0"
+}
---
+++ examples/resources/sci_corporate_idp/import.sh
@@ -0,0 +1,3 @@
+# terraform import sci_corporate_idp.<resource_name> <idp_id>
+
+terraform import sci_corporate_idp.saml2_example dd005d8b-1fee-4e6b-b6ff-cb9a197b7fe0
