190832dc8fb9c1bebf6a90d5b2891fa6f25ccc45
ravi-ydv <raviyadavrr37@gmail.com>
2026-04-09 05:53:05 +0000

    docs: added examples for sci_client_secret

---
+++ examples/data-sources/sci_client_secrets/data-source.tf
@@ -0,0 +1,3 @@
+data "sci_client_secrets" "example" {
+  application_id = sci_application.example.id
+}
---
+++ examples/resources/sci_client_secret/import.sh
@@ -0,0 +1,3 @@
+# terraform import sci_client_secret.<resource_name>  <application_id>,<id>
+
+terraform import sci_client_secret.my_app_secret dd005d8b-1fee-4e6b-b6ff-cb9a197b7fe0,dd005d8b-1fee-4e6b-b6ff-cb9a197b7fe0
