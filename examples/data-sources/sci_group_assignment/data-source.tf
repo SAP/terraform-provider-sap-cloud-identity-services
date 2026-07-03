5399892ba9cfce08b9d31efe4b44aaece73a8fd6
I584814 <diya.dhandapani.shanmugam@sap.com>
2026-07-03 14:04:18 +0530

    minor fix

--- examples/data-sources/sci_group_assignment/data-source.tf
+++ examples/data-sources/sci_group_assignment/data-source.tf
@@ -2,6 +2,6 @@
 data "sci_group_assignment" "by_id" {
   group_id = "group_1234567890" # Must be a valid UUID
   group_member = {
-    value = "user_1234567890"   # Must be a valid UUID
+    value = "user_1234567890" # Must be a valid UUID
   }
 }
--- examples/resources/sci_group_assignment/resource.tf
+++ examples/resources/sci_group_assignment/resource.tf
@@ -1,6 +1,6 @@
 # Assign a member to a group
 resource "sci_group_assignment" "basic_group_assignment" {
- group_id = "valid-uuid"
+  group_id = "valid-uuid"
   group_members = [
     {
       value = "valid-uuid",
