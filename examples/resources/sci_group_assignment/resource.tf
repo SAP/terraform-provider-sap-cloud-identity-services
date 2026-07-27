007b3555cb8d50d53286500855e20fbd2e87a4d3
I584814 <diya.dhandapani.shanmugam@sap.com>
2026-07-27 15:39:21 +0530

    format issue addressed

--- examples/resources/sci_group_assignment/resource.tf
+++ examples/resources/sci_group_assignment/resource.tf
@@ -2,7 +2,7 @@
 resource "sci_group_assignment" "basic_group_assignment" {
   group_id = "valid-uuid"
   group_member = {
-      value = "valid-uuid",
-      type  = "User" # Refer to the documentation for valid values
-    }
+    value = "valid-uuid",
+    type  = "User" # Refer to the documentation for valid values
+  }
 }
